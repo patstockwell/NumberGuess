430f618f7fdaf89a30f9641a26b9f830477f0308
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-03-10 16:55:40 +1100

    change input types for guess thread and add gameCount to return type

--- src/main/java/numberGuess/GuessController.java
+++ src/main/java/numberGuess/GuessController.java
@@ -14,9 +14,40 @@ public class GuessController {
 	
 	@CrossOrigin
 	@RequestMapping("/guess")
-	public Guess makeGuess(@RequestParam(value="number", defaultValue="99") String number) {
-		//put here all the logic to decide how 
-		//the guess affects the game and what kind of response to send
-		return new Guess(counter.incrementAndGet(), "the number guessed was:" + number, 32, -1, 78932);
+	public Guess makeGuess(
+			@RequestParam(value="number", defaultValue="99") String guessedNum,
+			@RequestParam(value="game", defaultValue="32") String gameCount,
+			@RequestParam(value="user", defaultValue="1211") String userID
+			) {
+		String message;
+		int currentPoints = 100; //currentPoints should initialise from the database.
+		int changeInPoints;
+		
+		//check guess matches the right game
+		if (Integer.parseInt(gameCount) != Arena.OFDOOM.getGameCount()) {
+			message = "Someone guessed the number before you. A new game has started, try again!";
+			changeInPoints = 0;
+		}
+		//guess too low
+		else if(Integer.parseInt(guessedNum) < Arena.OFDOOM.getGame().getSecretNum()) {
+			message = "Higher than " + guessedNum + ", try again";
+			changeInPoints = -1;
+		}
+		//guess too high
+		else if(Integer.parseInt(guessedNum) > Arena.OFDOOM.getGame().getSecretNum()) {
+			message = "Lower than " + guessedNum + ", try again";
+			changeInPoints = -1;
+		}
+		//guess correct
+		else {
+			Arena.OFDOOM.createNewGame();
+			message = "Winner! " + guessedNum + "is the secret Number";
+			changeInPoints = Integer.parseInt(guessedNum);
+		}
+		
+		currentPoints = currentPoints + changeInPoints;
+		//write info to the database
+	
+		return new Guess(counter.incrementAndGet(), message, currentPoints, changeInPoints, Integer.parseInt(userID));
 	}
 }

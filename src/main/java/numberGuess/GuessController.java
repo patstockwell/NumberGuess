ee0c97398337c4f6b6908255ee26e8861de7f17f
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-03-24 12:51:56 +1100

    create exception handling for slack posting, connect to database for recognising users, and add game/count service

--- src/main/java/numberGuess/GuessController.java
+++ src/main/java/numberGuess/GuessController.java
@@ -4,60 +4,72 @@ import java.util.concurrent.atomic.AtomicLong;
 import net.gpedro.integrations.slack.SlackApi;
 import net.gpedro.integrations.slack.SlackMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GuessController {
-
 	private final AtomicLong counter = new AtomicLong();
+	@Autowired
 	private PlayerRepository playerRepository;
-	
 	@CrossOrigin
 	@RequestMapping("/guess")
-	public GuessResponse makeGuess(@RequestParam(value="guessedNum", defaultValue="99") String guessedNum, @RequestParam(value="gameCount", defaultValue="-1") String gameCount, @RequestParam(value="userID", defaultValue="1211") String userID) {
+	public GuessResponse makeGuess(@RequestParam(value="guessedNum", defaultValue="101") String guessedNum, @RequestParam(value="gameCount", defaultValue="-1") String gameCount, @RequestParam(value="userID", defaultValue="1211") long userID) {
 		System.out.println(Arena.OFDOOM.getGame().getSecretNum()); //print number to terminal for debugging
-		
-		Player currentPlayer = playerRepository.findById(userID);
-		
+		Player currentPlayer; //are they logged in?
+		try {
+			System.out.println("Looking for user id: " + userID);
+			currentPlayer = playerRepository.findById(userID);
+			System.out.println("Found user id: " + userID);
+			if (currentPlayer == null) {
+				System.out.println("Current user null");
+				return new GuessResponse(0, "Invalid guess, user not logged in.", 0, 0, 0);
+			}
+		}
+		catch (Exception e) {
+			return new GuessResponse(0, "Invalid guess, user not logged in.", 0, 0, 0);
+		}
+		//set up some variables for the response
+		int guess = Integer.parseInt(guessedNum);
+		int secretNum = Arena.OFDOOM.getGame().getSecretNum();
 		String message;
-		int currentPoints = 100; //currentPoints should initialise from the database.
-		int changeInPoints;
-		
-		int guessResponse = Arena.OFDOOM.getGame().guessNumber(Integer.parseInt(guessedNum));
-		
+		int changeInPoints = -1;
+		int currentPoints = currentPlayer.getPoints();
 		//check guess matches the right game
-		if (isNotCurrentGame(Integer.parseInt(gameCount))) {
+		if (isNotCurrentGame(Integer.parseInt(gameCount)))
 			message = "Someone guessed the number before you. A new game has started, try again!";
-			changeInPoints = 0;
-		}
-
 		//guess too low
-		else if(Integer.parseInt(guessedNum) < Arena.OFDOOM.getGame().getSecretNum()) {
+		else if(guess < secretNum)
 			message = "Higher than " + guessedNum + ", try again";
-			changeInPoints = -1;
-		}
 		//guess too high
-		else if(Integer.parseInt(guessedNum) > Arena.OFDOOM.getGame().getSecretNum()) {
+		else if(guess > secretNum) 
 			message = "Lower than " + guessedNum + ", try again";
-			changeInPoints = -1;
-		}
 		//guess correct
 		else {
 			Arena.OFDOOM.createNewGame();
 			message = "Winner! " + guessedNum + " is the secret Number";
 			changeInPoints = Integer.parseInt(guessedNum);
+			try {
 			SlackApi api = new SlackApi("https://hooks.slack.com/services/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");
-			api.call(new SlackMessage("User:" + userID + "\n " + message + "\nChange in points: " + changeInPoints + "\nCurrent Points: " + currentPoints));
+			api.call(new SlackMessage("\nUser: " + currentPlayer.getName() + "\n " + message + "\nChange in points: " + changeInPoints + "\nCurrent Points: " + currentPoints + "\n"));
+			} 
+			catch (Exception e){
+				System.out.println("No Slack connection available.");
+			}
 		}
 		currentPoints = currentPoints + changeInPoints;
 		//write info to the database
+		currentPlayer.setPoints(currentPoints);
+		playerRepository.save(currentPlayer);
 		
 		
 	
-		return new GuessResponse(counter.incrementAndGet(), message, currentPoints, changeInPoints, Integer.parseInt(userID));
+		return new GuessResponse(counter.incrementAndGet(), message, currentPoints, changeInPoints, userID);
 	}
 	
 	public boolean isNotCurrentGame(int gameCount) {
@@ -66,5 +78,11 @@ public class GuessController {
 			isNotCurrentGame = false;
 		}
 		return isNotCurrentGame;
+	}
+	
+	@CrossOrigin
+	@GetMapping(path="/game/count")
+	public @ResponseBody int getGameCount() {
+		return Arena.OFDOOM.getGameCount();
 	}
 }
